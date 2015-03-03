0020f0effd89e97a8a96bdeb951a841e81c7d969
coggedwheel <fedorov@kwendi.net>
2015-03-03 21:40:49 +0200

    Test class added to test repository functions

---
+++ TryPhoneBook/src/TestClass.java
@@ -0,0 +1,20 @@
+public class TestClass {
+	String sText;
+	int iIndex;
+
+	public String getsText() {
+		return sText;
+	}
+
+	public void setsText(String sText) {
+		this.sText = sText;
+	}
+
+	public int getiIndex() {
+		return iIndex;
+	}
+
+	public void setiIndex(int iIndex) {
+		this.iIndex = iIndex;
+	}
+}
